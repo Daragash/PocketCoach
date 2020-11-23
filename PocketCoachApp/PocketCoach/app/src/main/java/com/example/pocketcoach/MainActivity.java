d48012a45b2125a98d1490fb83c52c9f77e0a22e
Nils Wagner <nilz.wagner@gmail.com>
2020-11-23 13:24:12 +0100

    hochladen der funktionierenden App

--- PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
+++ PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
@@ -8,14 +8,19 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
 
     TextView  textViewLogInStatus;     // to show the Log in Status of the user
+    ListView listViewExercise;         // ListView for the possible exercises
+    String[] exercisesArray;           // List of the possible exercises
 
     private FirebaseAuth firebaseAuth; // to check if the user is logged in
 
@@ -26,17 +31,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textViewLogInStatus = (TextView) findViewById(R.id.textViewLogInStatus); // initialization fot the Text View
+        textViewLogInStatus = (TextView) findViewById(R.id.textViewLogInStatus); // initialization of the TextView
+        listViewExercise = (ListView) findViewById(R.id.listViewExercise);       // initialization of the ListView
+        exercisesArray = getResources().getStringArray(R.array.exercises_array); // get the Array from res
+        firebaseAuth = FirebaseAuth.getInstance();                               // gets the log in status of the user
 
-        firebaseAuth = FirebaseAuth.getInstance(); // gets the log in status of the user
+        // initialization of the Array Adaper for the listViewExercise ListView
+        ArrayAdapter arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                exercisesArray);
+        listViewExercise.setAdapter(arrayAdapter); // Links ListView to the arrayAdapeter
 
+        // the following if else statement is used to checkif the user is logged in
+        // not logged in -> forward user to login
         if(firebaseAuth.getCurrentUser() == null) {
             textViewLogInStatus.setText(getString(R.string.not_logged_in));
 
-            //forward user to login
             Intent intent = new Intent(MainActivity.this, SignInActivity.class);
             startActivity(intent);
 
+        // logged in -> user gets access to the Main Activity
         }else {
             textViewLogInStatus.setText(getString(R.string.logged_in));
         }
