6c71e8a422776a696a8b97c8843a995db404d636
Nils Wagner <nilz.wagner@gmail.com>
2020-11-27 18:38:14 +0100

    Hinzufügen der RecordHistory Acitvitiy mit dem entsprechenden Button

--- PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
+++ PocketCoachApp/PocketCoach/app/src/main/java/com/example/pocketcoach/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     TextView textViewLogInStatus;      // to show the Log in Status of the user
     TextView textViewImprint;          // for the Impressum
     ListView listViewExercise;         // ListView for the possible exercises
+    Button   buttonRecordHistory;      // Button to open the RecordHistoryActivity
     String[] exercisesArray;           // List of the possible exercises
 
     private FirebaseAuth firebaseAuth; // to check if the user is logged in
@@ -36,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         textViewLogInStatus = (TextView) findViewById(R.id.textViewLogInStatus);      // initialization of the TextView for the LogIn Status
         textViewImprint     = (TextView) findViewById(R.id.textViewImprint);          // initialization of the TextView for the Imprint
         listViewExercise    = (ListView) findViewById(R.id.listViewExercise);         // initialization of the ListView
+        buttonRecordHistory = (Button)   findViewById(R.id.buttonRecordHistory);      // initialization of the Record History Button
         exercisesArray      = getResources().getStringArray(R.array.exercises_array); // get the Array from res
         firebaseAuth        = FirebaseAuth.getInstance();                             // gets the log in status of the user
 
@@ -111,6 +114,8 @@ public class MainActivity extends AppCompatActivity {
     /***********************************************************************************************
      * ON OPTION ITEM SELECTED
      * gives a function to the menu
+     * by pressing the menu the user is logged out
+     * after the log Out Process the user is forwarded to the SignInActivity
      **********************************************************************************************/
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -131,5 +136,15 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    /***********************************************************************************************
+     * ON CLICK BUTTON RECORD HISTORY
+     * if the Button buttonRecordHistory is pressed
+     * the user is forwarded to the RecordHistory Activity
+     **********************************************************************************************/
+    public void onClickButtonRecordHistory(View view){
+        Intent intentRecordHistory = new Intent(MainActivity.this, RecordHistory.class);
+        startActivity(intentRecordHistory);
     }
 }
